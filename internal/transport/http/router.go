72af62737b7a6ac4bbe1fc1ea1ecbb73dcfe81f0
ardrass2 <ardrass@mail.ru>
2026-04-07 23:02:33 +0300

    feat: register periodic-tasks routes in router

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -9,7 +9,11 @@ import (
 	httphandlers "example.com/taskservice/internal/transport/http/handlers"
 )
 
-func NewRouter(taskHandler *httphandlers.TaskHandler, docsHandler *swaggerdocs.Handler) *mux.Router {
+func NewRouter(
+	taskHandler *httphandlers.TaskHandler,
+	periodicTaskHandler *httphandlers.PeriodicTaskHandler,
+	docsHandler *swaggerdocs.Handler,
+) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/swagger/openapi.json", docsHandler.ServeSpec).Methods(http.MethodGet)
@@ -23,6 +27,12 @@ func NewRouter(taskHandler *httphandlers.TaskHandler, docsHandler *swaggerdocs.H
 	api.HandleFunc("/tasks/{id:[0-9]+}", taskHandler.GetByID).Methods(http.MethodGet)
 	api.HandleFunc("/tasks/{id:[0-9]+}", taskHandler.Update).Methods(http.MethodPut)
 	api.HandleFunc("/tasks/{id:[0-9]+}", taskHandler.Delete).Methods(http.MethodDelete)
+
+	api.HandleFunc("/periodic-tasks", periodicTaskHandler.Create).Methods(http.MethodPost)
+	api.HandleFunc("/periodic-tasks", periodicTaskHandler.List).Methods(http.MethodGet)
+	api.HandleFunc("/periodic-tasks/{id:[0-9]+}", periodicTaskHandler.GetByID).Methods(http.MethodGet)
+	api.HandleFunc("/periodic-tasks/{id:[0-9]+}", periodicTaskHandler.Update).Methods(http.MethodPut)
+	api.HandleFunc("/periodic-tasks/{id:[0-9]+}", periodicTaskHandler.Delete).Methods(http.MethodDelete)
 
 	return router
 }
